629c22c7bcb527d4dc6dd8aa5e15e47dc49dc8a4
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2019-10-23 01:58:32 +0330

    traverseTo() does what it should do now, fixed other methods accordingly

--- 4-Structures/src/structure/linkedlist/LinkedList.java
+++ 4-Structures/src/structure/linkedlist/LinkedList.java
@@ -13,16 +13,20 @@ public class LinkedList {
     public boolean isEmpty() { return (this.head == null); }
 
     public boolean delete(int index) {
-        if (this.isEmpty())
-            return false;
-        else if (index == 0) {
-            this.head = this.head.getNext();
-            return true;
-        } else {
-            Node prevNode = this.traverseTo(index - 1);
+        Node prevNode = this.traverseTo(index - 1);
+
+        if (prevNode == null) {
+            if (index != 0 || this.isEmpty())
+                return false;
+            else {
+                this.head = null;
+                return true;
+            }
+        } else if (prevNode.getNext() != null) {
             prevNode.setNext(prevNode.getNext().getNext());
             return true;
-        }
+        } else
+            return false;
     }
 
     public boolean delete() {
@@ -30,16 +34,14 @@ public class LinkedList {
             return false;
         else {
             Node currentNode = this.head;
-            Node nextNode = currentNode.getNext();
-            if (nextNode == null)
+            if (currentNode.getNext() == null)
                 this.head = null;
             else {
                 Node prevNode;
                 do {
                     prevNode = currentNode;
-                    currentNode = nextNode;
-                    nextNode = currentNode.getNext();
-                } while (nextNode != null);
+                    currentNode = currentNode.getNext();
+                } while (currentNode.getNext() != null);
                 prevNode.setNext(null);
             }
             return true;
@@ -54,13 +56,16 @@ public class LinkedList {
     }
 
     public boolean insert(Person person, int index) {
+        if (index == 0) {
+            this.head = new Node(person, this.head);
+            return true;
+        }
         Node prevNode = this.traverseTo(index - 1);
         if (prevNode != null) {
             prevNode.setNext(new Node(person, prevNode.getNext()));
             return true;
-        } else {
+        } else
             return false;
-        }
     }
 
     public Person get(int index) {
@@ -74,11 +79,13 @@ public class LinkedList {
     }
 
     private Node traverseTo(int index) {
-        Node currentNode = this.head;
+        Node currentNode = index < 0 ? null : this.head;
         if (currentNode != null) {
             Node nextNode = currentNode.getNext();
-            for (int i = 0; i < index && nextNode != null; i++) {
+            for (int i = 0; i < index; i++) {
                 currentNode = nextNode;
+                if (currentNode == null)
+                    break;
                 nextNode = currentNode.getNext();
             }
         }
