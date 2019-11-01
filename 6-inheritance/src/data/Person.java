763e6577a5b3e36b1e1d81054d9fddf8ff6cade0
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2019-11-02 00:44:38 +0330

    added inheritance module

---
+++ 6-inheritance/src/data/Person.java
@@ -0,0 +1,29 @@
+package data;
+
+public class Person {
+    private String name, address, email;
+
+    public Person(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Name:" + this.name +
+                "\tAddress:" + this.address +
+                "\tEmail:" + this.email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
