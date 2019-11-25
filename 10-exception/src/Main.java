0dbc3fc5894d27ed9f48dfd1e5905dc62b7e7f5d
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2019-11-25 12:56:59 +0330

    moved everything to a function

--- 10-exception/src/Main.java
+++ 10-exception/src/Main.java
@@ -5,26 +5,33 @@ import structure.QueueFullException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Queue<Integer> q = new Queue<Integer>(3);
-            try {
-                q.enqueue(1);
-                q.enqueue(2);
-                q.enqueue(3);
-                q.enqueue(4);
-            } catch (QueueFullException exception) {
-                System.err.println("Queue is full!");
-                exception.printStackTrace();
-            }
+            f();
+        } catch (QueueFullException e) {
+            System.err.println("Queue is full!");
+            e.printStackTrace();
+        } catch (QueueEmptyException e) {
+            System.err.println("Queue is empty!");
+            e.printStackTrace();
+        } finally {
+            System.out.println("No matter what, this will be executed!");
+        }
+    }
 
-            try {
-                System.out.println(q.dequeue());
-                System.out.println(q.dequeue());
-                System.out.println(q.dequeue());
-                System.out.println(q.dequeue());
-            } catch (QueueEmptyException exception) {
-                System.err.println("Queue is empty!");
-                exception.printStackTrace();
-            }
+    private static void f() throws QueueFullException, QueueEmptyException {
+        try {
+            Queue<Integer> q = new Queue<Integer>(3);
+
+            q.enqueue(1);
+            q.enqueue(2);
+            q.enqueue(3);
+            q.enqueue(4);
+
+
+            System.out.println(q.dequeue());
+            System.out.println(q.dequeue());
+            System.out.println(q.dequeue());
+            System.out.println(q.dequeue());
+
         } catch (NegativeArraySizeException exception) {
             System.err.println("Size is negative!");
             exception.printStackTrace();
